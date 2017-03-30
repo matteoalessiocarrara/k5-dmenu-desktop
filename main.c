876ce4a7d3413801ccbdddb3d8a11f2c8827c305
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2017-03-30 22:14:35 +0200

    Simplified code

--- main.c
+++ main.c
@@ -14,50 +14,55 @@
 #define die(...) {fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE);}
 #define mstrcpy(dest, src) {dest = malloc(strlen(src) + 1); strcpy(dest, src);}
 
-void find_files(int namesfd, int filesfd, char dirname[]) {
-	DIR *d = opendir(dirname);
+void find_files(char dirname[]) {
+	DIR *d;
+
+	d = opendir(dirname);
 	if (d == NULL) {
 		fprintf(stderr, "`%s`: %s\n", dirname, strerror(errno));
 		return;
 	}
 
-	errno = 0;
-	while (1) {
-		struct dirent *entry = readdir(d);
-		char fullpath[PATH_MAX];
+	for(errno = 0;;) {
+		struct dirent *entry;
+		char entry_path[PATH_MAX];
 
+		entry = readdir(d);
 		if (entry == NULL) {
-			if(errno) perror(NULL);
+			if(errno != 0) perror(NULL);
 			break;
 		}
 
-		strcpy(fullpath, dirname);
-		strcat(fullpath, "/");
-		strcat(fullpath, entry->d_name);
+		strcpy(entry_path, dirname);
+		strcat(entry_path, "/");
+		strcat(entry_path, entry->d_name);
 
 		// warning: d_type is non-standard
 		if (entry->d_type & DT_DIR) {
-			if (strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")) {
-				find_files(namesfd, filesfd, fullpath);
+			if ((strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0)) {
+				find_files(entry_path);
 			}
 		}
-		else if(strlen(entry->d_name) >= (sizeof(".desktop") - 1)) {
-			if (!strncmp(entry->d_name + (strlen(entry->d_name) - sizeof(".desktop") + 1), ".desktop", sizeof(".desktop"))) {
+		else if(strlen(entry->d_name) >= strlen(".desktop")) {
+			if (!strcmp(entry->d_name + (strlen(entry->d_name) - strlen(".desktop")), ".desktop")) {
 				const unsigned bufsize = 200;
 				char buf[bufsize + 1];
 				int fd;
 
-				if ((fd = open(fullpath, O_RDONLY)) == -1)
-					fprintf(stderr, "'%s': %s\n", fullpath, strerror(errno));
+				fd = open(entry_path, O_RDONLY);
+				if (fd == -1) {
+					fprintf(stderr, "'%s': %s\n", entry_path, strerror(errno));
+				}
 				else {
-					ssize_t r;
-					while((r = read(fd, &buf, bufsize)) > 0) {
-						unsigned broken_bytes = 0;
+					for(ssize_t r; (r = read(fd, &buf, bufsize)) > 0;) {
+						short broken_bytes = 0;
+
 						for(; buf[r - 1 - broken_bytes] != '\n'; broken_bytes++);
 						buf[r - broken_bytes] = '\0';
-					}
+						if(broken_bytes) lseek(fd, -broken_bytes, SEEK_CUR);
+						}
 
-					if(close(fd) == -1) fprintf(stderr, "'%s': %s", fullpath, strerror(errno));
+					if(close(fd) == -1) fprintf(stderr, "'%s': %s", entry_path, strerror(errno));
 				}
 			}
 		}
@@ -67,34 +72,42 @@ void find_files(int namesfd, int filesfd, char dirname[]) {
 }
 
 int main (int argc, char **argv) {
-	char **data_dirs = malloc(sizeof(char*));
-	short dir = 1;
+	char **searchdirs, *xdg_data_dirs;
+	short dirs;
 	
-	if ((data_dirs[0] = getenv("XDG_DATA_HOME")) == NULL) {
-		char *home = getenv("HOME");
+	searchdirs = malloc(sizeof(char*));
+	dirs = 1;
+	searchdirs[0] = getenv("XDG_DATA_HOME");
+	if (searchdirs[0] == NULL) {
+		char *home;
+
+		home = getenv("HOME");
 		if (home == NULL) die("Err: Unable to get $HOME value\n");
-		data_dirs[0] = malloc(strlen(home) + sizeof("/.local/share") + 1);
-		strcpy(data_dirs[0], home);
-		strcat(data_dirs[0], "/.local/share");
+
+		searchdirs[0] = malloc(strlen(home) + strlen("/.local/share") + 1);
+		strcpy(searchdirs[0], home);
+		strcat(searchdirs[0], "/.local/share");
 	}
 	
-	char *tmp = getenv("XDG_DATA_DIRS");
-	if (tmp == NULL) {
-		realloc(data_dirs, sizeof(char*) * (dir += 2));
-		mstrcpy(data_dirs[1], "/usr/local/share/");
-		mstrcpy(data_dirs[2], "/usr/share");
+	xdg_data_dirs = getenv("XDG_DATA_DIRS");
+	if (xdg_data_dirs == NULL) {
+		dirs += 2;
+		realloc(searchdirs, sizeof(char*) * dirs);
+		mstrcpy(searchdirs[1], "/usr/local/share/");
+		mstrcpy(searchdirs[2], "/usr/share");
 	} else {
-		for(char *tok = strtok(tmp, ":"); tok != NULL; tok = strtok(NULL, ":")) {
-			realloc(data_dirs, sizeof(char*) * ++dir);
-			mstrcpy(data_dirs[dir - 1], tok);
+		for(char *tok = strtok(xdg_data_dirs, ":"); tok != NULL; tok = strtok(NULL, ":")) {
+			dirs++;
+			realloc(searchdirs, sizeof(char*) * dirs);
+			mstrcpy(searchdirs[dirs - 1], tok);
 		}
 	}
 
-	for(short i = 0; i < dir; i++) {
-		char dirname[PATH_MAX];
-		strcpy(dirname, data_dirs[i]);
-		strcat(dirname, "/applications");
-		find_files(1, 1, dirname);
+	for(short i = 0; i < dirs; i++) {
+		char dir[PATH_MAX];
+		strcpy(dir, searchdirs[i]);
+		strcat(dir, "/applications");
+		find_files(dir);
 	}
 
 	return 0;
